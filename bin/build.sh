1b4be5b0a38433f4fe7b6813d3fa800bf45c527c
kw-00 <christopher.p.wojcik@gmail.com>
2026-01-11 07:09:18 -0500

    Add comment to build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -12,7 +12,10 @@ java -cp "${classpath}" java_cup.Main < "${base_path}/${src_path}/Parser.cup"
 
 cd "${base_path}"
 
+# Copy Main to the java folder, where it will be compiled 
+# together with the rest of the source files
 cp "${src_path}/Main.java" "${java_path}/Main.java"
+
 # Compile Main
 javac -d "${out_path}" -cp "${classpath}" -sourcepath "${java_path}" "${java_path}/Main.java" -Xlint:deprecation
 
